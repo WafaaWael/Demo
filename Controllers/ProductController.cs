1952a75ecb7cb30f2fa9892ad4f0757ddfe3cfe6
Wafaa Wael Farouq Ahmed <76271054+WafaaWael@users.noreply.github.com>
2025-04-26 02:56:05 +0300

    controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string ShowMsg()
+        {
+            return "hello from frist Action";
+        }
+        public ContentResult ShowMsg2()
+        {
+            ContentResult contentResult = new ContentResult();
+            contentResult.Content = "local msg";
+            return contentResult;
+        }
+        public ViewResult ShowView()
+        {
+            ViewResult viewResult = new ViewResult();
+            viewResult.ViewName = "View1";
+            return viewResult;
+        }
+        public JsonResult ShowJson()
+        {
+            JsonResult result = new JsonResult(new { ID = 1, Name = "Ahmed" });
+            return result;
+        }
+    }
+}
